ef6457dc584160e9a760ab90dd47d5b81ae7b128
asherbar <asherbare@gmail.com>
2019-01-14 12:27:48 +0200

    Added ability to query table

--- inspector_db_app/logic/table.py
+++ inspector_db_app/logic/table.py
@@ -10,11 +10,16 @@ class TableDoesNotExist(Exception):
 
 
 class Table:
-    def __init__(self, table_name):
+    def __init__(self, table_name, limit=50):
         self._table_name = table_name
+        self._limit = limit
         if not self._table_exists():
             logger.info('Table %s not found', table_name)
             raise TableDoesNotExist(table_name)
+
+    @property
+    def table_name(self):
+        return self._table_name
 
     def get_columns(self):
         with get_connection() as c:
@@ -22,11 +27,17 @@ class Table:
             cursor.execute('SELECT * FROM {} LIMIT 1'.format(self._table_name))
             return [desc[0] for desc in cursor.description]
 
-    def get_records(self, limit):
+    def get_all_records(self):
         with get_connection() as c:
             cursor = c.cursor()
-            cursor.execute('SELECT * FROM {} LIMIT {}'.format(self._table_name, limit))
-            return cursor.fetchmany(limit)
+            cursor.execute('SELECT * FROM {} LIMIT {}'.format(self._table_name, self._limit))
+            return cursor.fetchmany(self._limit)
+
+    def execute_query(self, query):
+        with get_connection() as c:
+            cursor = c.cursor()
+            cursor.execute(query)
+            return cursor.fetchmany(self._limit)
 
     def _table_exists(self):
         with get_connection() as c:
--- inspector_db_app/views.py
+++ inspector_db_app/views.py
@@ -1,9 +1,13 @@
+import psycopg2
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, Http404
 from django.template import loader
 
+from inspector_db_app.logic.logger_utils import get_logger
 from inspector_db_app.logic.table import Table, TableDoesNotExist
 from inspector_db_app.logic.tables import Tables
+
+logger = get_logger(__name__)
 
 
 @login_required
@@ -24,10 +28,35 @@ def get_table(request, table_name):
     except TableDoesNotExist:
         raise Http404("Table {} does't exist".format(table_name))
     tables = Tables()
-    context = {
-        'column_names': table.get_columns(),
-        'records': table.get_records(50),
-        'table_names': tables.get_public_tables(),
-        'current_table_name': table_name,
-    }
+    if 'query' in request.GET:
+        context = _get_query_context(request.GET['query'], table, tables)
+    else:
+        context = _get_table_context(table, tables)
     return HttpResponse(template.render(context, request))
+
+
+def _get_query_context(query, table, tables):
+    table_title = query
+    try:
+        records = table.execute_query(query)
+    except psycopg2.ProgrammingError:
+        table_title = 'Illegal query: {}'.format(query)
+        logger.exception(table_title)
+        records = table.get_all_records()
+    return {
+        'column_names': table.get_columns(),
+        'records': records,
+        'table_names': tables.get_public_tables(),
+        'current_table_name': table.table_name,
+        'table_title': table_title,
+    }
+
+
+def _get_table_context(table, tables):
+    return {
+        'column_names': table.get_columns(),
+        'records': table.get_all_records(),
+        'table_names': tables.get_public_tables(),
+        'current_table_name': table.table_name,
+        'table_title': 'All',
+    }
